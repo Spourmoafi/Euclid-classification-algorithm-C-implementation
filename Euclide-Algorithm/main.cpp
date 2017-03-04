de5d7bb566f8a18e5c09ae3bdd393e960dcc925e
Sajid Fadlelseed <sajid-home@Sajids-iMac.local>
2017-03-04 21:02:50 +0000

    Initial Commit

---
+++ Euclide-Algorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Euclide-Algorithm
+//
+//  Created by SajidQurashi on 04/03/2017.
+//  Copyright © 2017 SajidQurashi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
